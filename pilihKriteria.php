0e679184aefe459bfd490dd75bcb7481cad72220
Arima1213 <arima3992@gmail.com>
2024-12-06 22:01:52 +0700

    Format code in pilihKriteria.php for improved readability and consistency

--- pilihKriteria.php
+++ pilihKriteria.php
@@ -19,9 +19,14 @@ include_once 'connection.php';
         session_start();
         ?>
         <div class='col-2 d-flex flex-column justify-content-start h-100 py-5' id='sidebar'>
-            <button class='btn btn-light' onclick="location.href = 'MasterLaptop.php'" <?php if($_SESSION['jenis'] == "usr"){ echo "disabled"; } ?>>Master Laptop</button>
+            <button class='btn btn-light' onclick="location.href = 'MasterLaptop.php'" <?php if ($_SESSION['jenis'] == "usr") {
+                                                                                            echo "disabled";
+                                                                                        } ?>>Master Laptop</button>
             <button class='btn btn-light' onclick="location.href = 'MasterKriteria.php'">Master Kriteria</button>
-            <button class='btn btn-light' onclick="location.href = 'MasterKaryawan.php'" <?php if($_SESSION['jenis'] == "usr"){ echo "disabled"; } ?>>Master Karyawan</button>
+            <button class='btn btn-light' onclick="location.href = 'MasterKaryawan.php'" <?php if ($_SESSION['jenis'] == "usr") {
+                                                                                                echo "disabled";
+                                                                                            } ?>>Master
+                Karyawan</button>
             <div class='sidebarDivider align-self-center'></div>
             <button class='btn btn-primary' id='buttonHitung'>Hitung</button>
             <button class='btn btn-light' id='buttonHitung' onclick="location.href = 'logout.php'">Logout</button>
@@ -48,25 +53,27 @@ include_once 'connection.php';
                         $num = 1;
                         while ($data = mysqli_fetch_array($res)) {
                         ?>
-                            <form action="cek_konsistensi.php" method="post">
-                                <tr>
-                                    <input type="hidden" <?= 'name=id_kriteria' . $num ?> value="<?= $data['ID_KRITERIA'] ?>">
-                                    <th scope="row"> <?= $num; ?> </th>
-                                    <td> <?= $data['NAMA_KRITERIA'] ?> </td>
-                                    <td>
-                                        <select <?= 'name=prioritas' . $num ?> class='form-control w-50' style='font-weight: 600'>
-                                            <option value="1" style='font-weight: 600'>1 (Sangat Rendah)</option>
-                                            <option value="2">2</option>
-                                            <option value="3" style='font-weight: 600'>3 (Rendah)</option>
-                                            <option value="4">4</option>
-                                            <option value="5" style='font-weight: 600'>5 (Normal)</option>
-                                            <option value="6">6</option>
-                                            <option value="7" style='font-weight: 600'>7 (Penting)</option>
-                                            <option value="8">8</option>
-                                            <option value="9" style='font-weight: 600'>9 (Sangat Penting)</option>
-                                        </select>
-                                    </td>
-                                </tr>
+                        <form action="cek_konsistensi.php" method="post">
+                            <tr>
+                                <input type="hidden" <?= 'name=id_kriteria' . $num ?>
+                                    value="<?= $data['ID_KRITERIA'] ?>">
+                                <th scope="row"> <?= $num; ?> </th>
+                                <td> <?= $data['NAMA_KRITERIA'] ?> </td>
+                                <td>
+                                    <select <?= 'name=prioritas' . $num ?> class='form-control w-50'
+                                        style='font-weight: 600'>
+                                        <option value="1" style='font-weight: 600'>1 (Sangat Rendah)</option>
+                                        <option value="2">2</option>
+                                        <option value="3" style='font-weight: 600'>3 (Rendah)</option>
+                                        <option value="4">4</option>
+                                        <option value="5" style='font-weight: 600'>5 (Normal)</option>
+                                        <option value="6">6</option>
+                                        <option value="7" style='font-weight: 600'>7 (Penting)</option>
+                                        <option value="8">8</option>
+                                        <option value="9" style='font-weight: 600'>9 (Sangat Penting)</option>
+                                    </select>
+                                </td>
+                            </tr>
                             <?php
                             $num++;
                         }
